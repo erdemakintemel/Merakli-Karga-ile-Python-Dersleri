43abb41c194d16674e93f9f82f4f10b491b7f174
erdemakintemel <erdemakintemel@gmail.com>
2020-09-09 18:08:46 +0300

    yüce agustus'un gün hesaplayıcısı

---
+++ yuce_agustus.py
@@ -0,0 +1,17 @@
+ocak="31"
+şubat="28 ya da 29"
+mart="31"
+nisan="30"
+mayıs="31"
+haziran="30"
+temmuz="31"
+ağustos="31"
+eylül="30"
+ekim="31"
+kasım="30"
+aralık="31"
+
+a=input("bir ay ismi girin ve size kaç gün olduğunu hesaplayalım: ")
+ay=eval(a)
+
+print(ay)
